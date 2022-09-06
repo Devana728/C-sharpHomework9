e07358d24d9b09c332095ed18f45edf6dcf84d4d
devana <devana728@rambler.ru>
2022-09-06 03:45:20 +0300

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string message)
+{
+    System.Console.Write(message); //вывести сообщение
+
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void ShowNaturalRow(int number, int n)
+{
+    if (number <= n-1)
+    {
+        return;
+    }
+    ShowNaturalRow(number-1, n);
+    if (number%2 ==0)
+    System.Console.WriteLine(number);
+    
+}
+
+int n = Prompt("Введите число 1 : ");
+int number = Prompt("Введите число 2 : ");
+ShowNaturalRow(number, n);
+
+
---
+++ task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8 -> 30
+int Prompt(string message)
+{
+    System.Console.Write(message); //вывести сообщение
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNaturalRow(int start, int end)
+{
+
+    if (start == end)
+    {
+        return end;
+    }    
+   
+
+    else return start + SumNaturalRow(start+1, end);
+      
+    
+}
+
+int n = Prompt("Введите число 1 : ");
+int number = Prompt("Введите число 2 : ");
+int sum = SumNaturalRow(n, number);
+System.Console.WriteLine(sum);
+
+
